753643000e51ac275e9a0ea006a8f4e2cea8724f
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-20 21:27:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,22 @@
-placeholder
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['preact'],
+      output: {
+        globals: {
+          preact: 'Preact',
+        },
+      },
+    },
+    cssCodeSplit: true,
+  },
+});
